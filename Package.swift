d94d9037dd41e7c28d0b70229615ef5e6e2fae8c
metalurgical <97008724+metalurgical@users.noreply.github.com>
2024-01-17 06:36:07 +0200

    fix: reduce minimum ios target

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "curvelib.swift",
     platforms: [
-        .iOS(SupportedPlatform.IOSVersion.v14), .macOS(.v10_15)
+        .iOS(SupportedPlatform.IOSVersion.v13), .macOS(.v10_15)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
